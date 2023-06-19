b2dc18809a98d0424ee5d6a4e84d09be26da5fec
Sandro Ptak <sandroptak@gmail.com>
2023-06-19 16:38:26 -0300

    Front-end e inserido o componente de video

---
+++ src/components/pages/Video.js
@@ -0,0 +1,34 @@
+import { useRef, useState } from 'react'
+import styles from "./Video.module.css"
+
+function Video() {
+
+    const videoRef = useRef(null)
+    const [play, setPlay] = useState(false)
+
+    function handleStart() {
+        if (play) {
+            videoRef.current.pause()
+            setPlay(false)
+        } else {
+            videoRef.current.play()
+            setPlay(true)
+        }
+    }
+
+    return (
+        <div className={styles.video}>
+            <video
+                className={styles.video__player}
+                ref={videoRef}
+                onClick={handleStart}
+                loop
+                src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+            >
+
+            </video>
+        </div>
+    )
+}
+
+export default Video
